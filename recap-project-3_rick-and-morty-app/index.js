e2fdbdd9e820b07fb5df065f8b47b8f3eab3c2ec
meShailu <123243572+meShailu@users.noreply.github.com>
2023-07-25 13:04:41 +0200

    adding card component and fetch data

--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,3 +1,6 @@
+import { createCharacterCard } from "./components/card/card.js";
+import { fetchCharacters } from "./components/card/data.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +15,17 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+function createCharacterCards(characters) {
+  const characterCards = characters.map(createCharacterCard);
+  characterCards.forEach((card) => {
+    cardContainer.appendChild(card);
+  });
+}
+async function main() {
+  // Fetch character data from API
+  const characters = await fetchCharacters();
+  // Create and append character cards
+  createCharacterCards(characters);
+}
+main();
