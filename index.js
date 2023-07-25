f56540258c2b00de640695ce45cbdff76782e6e5
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-07-25 16:19:21 +0200

    pagination is crazy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { createCharacterCard } from "./components/card/card.js";
-import { fetchCharacters } from "./components/card/data.js";
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -10,9 +9,17 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 // States
-const maxPage = 1;
-const page = 1;
+const maxPage = 42;
+let page = 1;
 const searchQuery = "";
+
+async function fetchCharacters(pageIndex) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${pageIndex}`
+  );
+  const data = await response.json();
+  return data.results;
+}
 function createCharacterCards(characters) {
   const characterCards = characters.map(createCharacterCard);
   characterCards.forEach((card) => {
@@ -21,8 +28,29 @@ function createCharacterCards(characters) {
 }
 async function main() {
   // Fetch character data from API
-  const characters = await fetchCharacters();
+  const characters = await fetchCharacters(page);
   // Create and append character cards
   createCharacterCards(characters);
 }
 main();
+pagination.textContent = `${page} of ${maxPage}`;
+nextButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+  if (page < maxPage) {
+    page++;
+    pagination.textContent = `${page} of ${maxPage}`;
+    const characters = await fetchCharacters(page);
+    cardContainer.innerHTML = ""; // Clear previous cards
+    createCharacterCards(characters);
+  }
+});
+prevButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+  if (page > 1) {
+    page--;
+    pagination.textContent = `${page} of ${maxPage}`;
+    const characters = await fetchCharacters(page);
+    cardContainer.innerHTML = ""; // Clear previous cards
+    createCharacterCards(characters);
+  }
+});
